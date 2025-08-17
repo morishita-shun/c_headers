459591c088eee52ba65c5264f699242cf83a7e1d
morishita-shun <morishita-shun@protonmail.com>
2025-08-17 12:50:31 +0900

    add mirai.h

---
+++ mirai.h
@@ -0,0 +1,18 @@
+struct table_value_m68k {
+    char *val;
+    ushort val_len;
+};
+
+struct table_value_16bit {
+    char *val;
+    ushort val_len;
+    ushort padding;
+};
+
+struct table_value_32bit {
+    char *val;
+    ushort val_len;
+    ushort padding1;
+    ushort padding2;
+    ushort padding3;
+};
