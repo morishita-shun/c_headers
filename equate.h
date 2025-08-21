7a4ddde37402951839281615e62c59a1f9affb70
morishita-shun <morishita-shun@protonmail.com>
2025-08-22 08:08:24 +0900

    add signal equate

--- equate.h
+++ equate.h
@@ -1,4 +1,11 @@
-/* socket.h */
+/* socket */
 #define MSG_NOSIGNAL        0x4000
 #define MSG_PEEK            0x0002
 #define MSG_NOSIGNAL_PEEK   (MSG_NOSIGNAL | MSG_PEEK)
+
+/* signal */
+#define EINVAL              0x16
+#define SIGRTMAX            0x40
+#define NSIG                0x41
+#define SA_RESTART          0x10000000
+#define MASK_SA_RESTORER    0x4000000
